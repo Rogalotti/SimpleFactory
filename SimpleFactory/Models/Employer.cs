96317359a9e1f547faf74099938c400bbc74cee7
lzynda <lukaszzynda1@gmail.com>
2018-09-20 20:56:24 +0200

    Edit Eployer class

--- SimpleFactory/Models/Employer.cs
+++ SimpleFactory/Models/Employer.cs
@@ -7,11 +7,12 @@ namespace SimpleFactory.Models
 {
     public class Employer
     {
-        public int EmployerId { get; set; }
-        //foreign key factory
-        public int FactoryId { get; set; } 
+        public int EmployerId { get; set; }        
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string PhoneNumber { get; set; }
+
+        //foreign key factory
+        public int FactoryId { get; set; }
     }
 }
